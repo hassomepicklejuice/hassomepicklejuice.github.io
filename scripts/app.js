886f31aeb5068e3b1472d4ac166c2165c1e1a639
simvandew <90385129+simvandew@users.noreply.github.com>
2022-05-23 16:01:33 +0200

    dummy site

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const switcher = document.querySelector('.themeBtn');
+
+switcher.addEventListener('click', function() {
+    const className = document.body.className;
+    switch (className) {
+        case "light-theme":
+            document.body.classList.toggle('light-theme');
+            document.body.classList.toggle('dark-theme');
+            this.textContent = "LIGHT"
+            break;
+        case "dark-theme":
+            document.body.classList.toggle('light-theme');
+            document.body.classList.toggle('dark-theme');
+            this.textContent = "DARK"
+            break;
+        default:
+            document.body.classList.add('light-theme');
+            document.body.classList.remove('dark-theme');
+            this.textContent = "DARK"
+            break;
+    }
+});
