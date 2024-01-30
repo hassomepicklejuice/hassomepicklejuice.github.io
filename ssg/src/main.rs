419dd36c9c688881ec9129ce164a40700a27d124
hassomepicklejuice <hassomepicklejuice@proton.me>
2024-01-30 15:22:02 +0100

    ssg add command line args

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -1,47 +1,97 @@
-use std::{fs, path::Path};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
+use anyhow::{anyhow, Result};
+use clap::Parser;
 use handlebars::Handlebars;
-use toml::Table;
+use toml::{Table, Value};
+
+/// Custom static site generator.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Paths to Handlebars template files or directories containing template files
+    #[arg(short, long)]
+    templates: Vec<PathBuf>,
+    /// Path to the output directory, defaults to the current directory
+    #[arg(short, long)]
+    out_dir: Option<PathBuf>,
+    /// Paths to the files to be rendered, or directories containing the source files
+    files: Vec<PathBuf>,
+}
 
 fn main() {
+    let args = Args::parse();
+
+    assert!(!args.files.is_empty());
+
+    fs::create_dir_all(args.out_dir.unwrap_or_else(|| PathBuf::from(".")))
+        .expect("Created output directory");
+
     let mut handlebars = Handlebars::new();
-    handlebars
-        .register_templates_directory("templates", Default::default())
-        .expect("Registering templates");
 
-    let mut sources = std::env::args();
-    sources.next();
-
-    for source in sources {
-        let meta = read_source(source).expect("Reading source");
-
-        let template = meta["template"].as_str().expect("'template' is a string");
-        let typ = meta["type"].as_str().expect("'type' is a string");
-
-        match typ {
-            "html" => {
-                let rendered = handlebars
-                    .render(template, &meta)
-                    .expect("Rendering source");
-                eprintln!("{rendered}");
-            }
-            _ => {
-                eprintln!("Cannot handle files of type {typ} yet");
-            }
+    for template in args.templates {
+        if template.is_file() {
+            let name = match template.file_stem().and_then(|name| name.to_str()) {
+                Some(name) => name,
+                _ => continue,
+            };
+            handlebars
+                .register_template_file(name, &template)
+                .expect("Registering template file");
+        } else if template.is_dir() {
+            handlebars
+                .register_templates_directory(template, Default::default())
+                .expect("Registering template directory");
         }
+    }
+
+    for file in args.files {
+        let mut data = read_source(file).expect("Reading source");
+
+        parse_body(&mut data).expect("Body was parsed succesfully");
+
+        let template = data["template"]
+            .as_str()
+            .expect("'template' should be a string");
+
+        let rendered = handlebars
+            .render(template, &data)
+            .expect("Rendering source");
     }
 
     println!("Hello, world!");
 }
 
-fn read_source(source: impl AsRef<Path>) -> Result<Table, std::io::Error> {
-    let content = fs::read_to_string(source)?;
+fn parse_body(data: &mut Table) -> Result<()> {
+    match data["type"] {
+        Value::String(ref typ) if typ == "html" => Ok(()),
+        Value::String(ref typ) => Err(anyhow!("Cannot handle files of type {typ} yet")),
+        ref x => Err(anyhow!(
+            "Expected the 'type' field to be a String. Instead it was {x:?}"
+        )),
+    }
+}
+
+fn read_source(source: impl AsRef<Path>) -> Result<Table> {
+    let content = fs::read_to_string(source.as_ref())?;
     let (meta, body) = content.split_once("***\n").unwrap_or(("", &content));
-    let mut meta = meta.parse::<Table>().unwrap_or_default();
+    let mut data = meta.parse::<Table>().unwrap_or_default();
 
-    meta.entry("template").or_insert("article".into());
-    meta.entry("type").or_insert("html".into());
-    meta.insert("BODY".to_string(), body.into());
+    data.entry("template").or_insert("article".into());
 
-    Ok(meta)
+    data.entry("type").or_insert(
+        source
+            .as_ref()
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .ok_or(anyhow!("No extension present"))?
+            .into(),
+    );
+
+    data.insert("BODY".to_string(), body.into());
+
+    Ok(data)
 }
